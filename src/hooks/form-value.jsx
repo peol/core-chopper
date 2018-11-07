9719678f13e4c99f1e16393a39041181d44ef8bb
Andrée Hansson <peolanha@gmail.com>
2018-11-07 19:48:40 +0100

    Add extra truthy check when validating form

--- src/hooks/form-value.jsx
+++ src/hooks/form-value.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 export default function useFormValue(initialValue) {
   const [val, setVal] = useState(initialValue);
   return [val, (data) => {
-    if (data.target) {
+    if (data && data.target) {
       const valueProp = data.target.type === 'checkbox' ? 'checked' : 'value';
       setVal(data.target[valueProp]);
     } else {
