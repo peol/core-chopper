5e3dad25309359ab70d18969b9023231eaa8d7e2
Johan Bjerning <johan@bjerning.se>
2018-11-14 09:20:42 +0100

    Fixed merge issues

--- src/game/engine.jsx
+++ src/game/engine.jsx
@@ -5,8 +5,8 @@ import useModel from '../hooks/model';
 import useLayout from '../hooks/layout';
 import Chopper from './chopper';
 import Cloud from './cloud';
-import Floor from './floor';
 import FlyingObject from './flying-object';
+import Floor from './floor';
 import Resources from './resources';
 import Settings from './settings';
 import CountDown from './count-down';
@@ -70,7 +70,7 @@ export default function ({ player, socket }) {
       displayMode: ex.DisplayMode.Container,
       canvasElementId: canvasId,
       suppressConsoleBootMessage: true,
-      isDebug: true,
+      // isDebug: true,
       backgroundColor: ex.Color.Transparent,
     });
     engine.setAntialiasing(false);
@@ -99,6 +99,9 @@ export default function ({ player, socket }) {
         });
       };
       chopperRef.current = chopper;
+      for (let i = 0; i < Settings.OBEJCTS_OVER_CHOPPER; i += 1) {
+        engine.add(new FlyingObject(engine, chopper));
+      }
       engine.add(chopper);
       engine.add(countDown);
       engine.add(gameOver);
@@ -108,57 +111,16 @@ export default function ({ player, socket }) {
       for (let i = 0; i < engine.drawWidth; i += 61 * Settings.scale.y) {
         engine.add(new Floor(i, 0));
       }
+
+      for (let i = 0; i < Settings.OBEJCTS_UNDER_CHOPPER; i += 1) {
+        engine.add(new FlyingObject(engine, chopper));
+      }
       engine.add(hud);
       engine.input.pointers.primary.on('down', () => {
         chopper.bounce(250);
-
-    engine.currentScene.createGroup('chopper');
-    const scale = (engine.drawHeight / 800) * 0.50 + 1;
-    Object.assign(Settings.scale, { x: scale, y: scale });
-    const chopper = new Chopper(engine);
-    const countDown = new CountDown(engine, chopper);
-    const hud = new HUD(engine, chopper);
-    const gameOver = new GameOver(engine, chopper);
-    countDown.go = () => {
-      socket.send({
-        type: 'game:create',
-        data: {
-          userid: player.userid,
-        },
       });
-    };
-    gameOver.go = () => {
-      socket.send({
-        type: 'game:end',
-        data: {
-          gameid: gameid.current,
-          score: hud.maxScore,
-          bonus: chopper.kills,
-        },
-      });
-    };
-    chopperRef.current = chopper;
-
-    for (let i = 0; i < Settings.OBEJCTS_UNDER_CHOPPER; i += 1) {
-      engine.add(new FlyingObject(engine, chopper));
-    }
-    engine.add(chopper);
-    engine.add(countDown);
-    engine.add(gameOver);
-    engine.add(new Cloud(800, 0));
-    engine.add(new Cloud(400, 300 * Settings.scale.y));
-    engine.add(new Cloud(700, 700 * Settings.scale.y));
-    for (let i = 0; i < engine.drawWidth; i += 61 * Settings.scale.y) {
-      engine.add(new Floor(i, 0));
-    }
-    for (let i = 0; i < Settings.OBEJCTS_OVER_CHOPPER; i += 1) {
-      engine.add(new FlyingObject(engine, chopper));
-    }
-    engine.add(hud);
-    engine.input.pointers.primary.on('down', () => {
-      chopper.bounce(250);
+      engine.currentScene.camera.strategy.lockToActor(chopper);
     });
-    engine.currentScene.camera.strategy.lockToActor(chopper);
   }, [layout]);
 
   return <div className="engine"><canvas id={canvasId} /></div>;
