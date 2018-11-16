beac7a464147253478ed68297242bcf58452a73a
Mattias Wennmo <mow@qlik.com>
2018-11-16 16:13:45 +0100

    Make it possible to skip nfc support

--- server/sensors/nfc.js
+++ server/sensors/nfc.js
@@ -1,7 +1,18 @@
 const { NFC } = require('nfc-pcsc');
 
 const listeners = [];
-const nfc = new NFC();
+
+const logger = {
+  log: (...args) => (console.log(args)),
+  debug: (...args) => (console.log(args)),
+  info: (...args) => (console.log(args)),
+  warn: (...args) => (console.log(args)),
+  error: (...args) => (console.log(args)),
+};
+
+console.log('reader:starting');
+const nfc = new NFC(logger);
+console.log('reader:started');
 
 nfc.on('reader', (reader) => {
   console.log('reader:attached');
